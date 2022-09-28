844d39bf2d83ddf93f4400a009b79748dec56d5c
username <jafar.karimi121@gmail.com>
2022-09-28 17:18:51 -0500

    simple bais project

---
+++ src/class01codes/Class01Codes.java
@@ -0,0 +1,11 @@
+package class01codes;
+
+public class Class01Codes {
+
+	public static void main(String[] args) {
+		// code to launch the nuclear weapon
+		System.out.println("launch my fac=vourite Tv show");
+
+	}
+
+}
